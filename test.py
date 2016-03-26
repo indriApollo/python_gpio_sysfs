19d903d1c95f0e3649f0646a025e2b74b5ba123a
Raphael Leroux <indri.apollo@gmail.com>
2016-03-26 15:25:27 +0100

    Allow to set gpio number (BCM)

--- test.py
+++ test.py
@@ -3,10 +3,12 @@
 #blink RPI gpio0 (BCM17)
 from time import sleep
 from gpio import GPIO
-gp = GPIO(17,0,"out")
 
+p = input("gpio n : ")
 t = input("sleep time : ")
 m = input("max loop range : ")
+
+gp = GPIO(int(p),0,"out")
 
 for i in range(0,int(m)):
 	print("write 1")
